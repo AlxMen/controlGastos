f854147e94e3834329f02b0f1f11fba0bd57f3d8
AlxMen <alejandrocs402@gmail.com>
2023-04-05 00:26:09 +0100

    creacion de modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,23 @@
 import { useState } from 'react'
 import Header from './components/Header'
+import IconoNuevoGasto from './img/nuevo-gasto.svg'
+import Modal from './components/Modal'
 
 function App() {
 
   const [presupuesto, setPresupuesto] = useState(0)
   const [isValidPresupuesto, setIsValidPresupuesto] = useState(false)
+
+  const [modal, setModal] = useState(false)
+  const [animarModal, setAnimarModal] = useState(false)
+
+  const handleNuevoGasto = () =>{
+    setModal(true)
+
+    setTimeout(() => {
+      setAnimarModal(true)
+    }, 300);
+  }
 
   return (
     <div>
@@ -14,6 +27,23 @@ function App() {
         isValidPresupuesto={isValidPresupuesto}
         setIsValidPresupuesto={setIsValidPresupuesto}
       />
+
+      {isValidPresupuesto && (
+        <div className='nuevo-gasto'>
+          <img 
+            src={IconoNuevoGasto} 
+            alt="icono nuevo gasto" 
+            onClick={handleNuevoGasto}
+          />
+        </div>
+      )}
+      
+      {modal && <Modal 
+                  setModal={setModal}
+                  animarModal={animarModal}
+                  setAnimarModal={setAnimarModal}
+                />}
+
     </div>
   )
 }
