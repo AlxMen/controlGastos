da4557e7d28907b9f00ca585787bc285e5118a4f
AlxMen <alejandrocs402@gmail.com>
2023-04-10 18:28:50 +0100

    finalizacion del proyecto gastos

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -1,5 +1,5 @@
 
-export const generarTd = () => {
+export const generarId = () => {
   const random = Math.random().toString(36).substring(2)
   const fecha = Date.now().toString(36)
   return random + fecha
